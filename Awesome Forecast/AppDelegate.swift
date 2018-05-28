6943daf51ea9753d19760a7adb83b27b67bd34b2
Sergey Bendak <sleepoke@gmail.com>
2018-05-28 21:17:14 +0300

    Implement basic configuration for FBSDK

--- Awesome Forecast/AppDelegate.swift
+++ Awesome Forecast/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 typealias VoidClosure = () -> ()
 
@@ -19,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         UIApplication.shared.statusBarStyle = .lightContent
         
         let tabBarController = TabBarBuilder.build(networkManager: networkManager)
@@ -27,6 +30,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        return handled
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
